2ae60dc3bd1a275702c5809fc9b2b6eb17770fbf
iratao <iratao@gmail.com>
2018-01-30 21:38:36 +0800

    更新新年计划

--- src/js/containers/plan.js
+++ src/js/containers/plan.js
@@ -11,9 +11,10 @@ export default class Plan extends Component {
         <ul className={styles.list}>
           <li>听完《薛兆丰的北大经济学课》</li>
           <li>听完《蒋勋细说红楼梦》</li>
-          <li>看完《煮酒谈西游》</li>
+          <li>看完《煮酒探西游》</li>
           <li>看完《金瓶梅》</li>
           <li>看完《三体》</li>
+          <li>坚持一年不换手机</li>
         </ul>
       </div>
     )
